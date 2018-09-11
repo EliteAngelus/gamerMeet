aa801bc4faca5226c0e3c0ab94de8134d4743c89
EliteAngelus <Angeluselite@gmail.com>
2018-09-10 21:34:43 -0400

    Edited CSS on signup.html

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+var path = require('path');
+const bodyparser = require('body-parser');
+
+// DataBase 
+// First Name 
+// Last Name 
+// Username 
+// Email 
+// Password 
+
+
+app.listen(3000, function() {
+    console.log("Listening on ")
+})
